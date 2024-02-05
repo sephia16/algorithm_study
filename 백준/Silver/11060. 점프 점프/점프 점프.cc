9c902d483165e1bb2d453ce1c356112f8ff4c420
sephia16 <149568715+sephia16@users.noreply.github.com>
2024-02-05 20:22:18 +0900

    Update 점프 점프.cc

--- 백준/Silver/11060. 점프 점프/점프 점프.cc
+++ 백준/Silver/11060. 점프 점프/점프 점프.cc
@@ -1,4 +1,34 @@
 
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// #include <climits>
+
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     vector<int> a(n);
+//     for(int i=0; i<n; i++){
+//         cin >> a[i];
+//     }
+//     vector<int> dp(n,-1);
+//     dp[0] = 0;
+//     for(int i=1; i<n; i++){
+//         for(int j=0; j<i; j++){
+//             if(dp[j] != -1 && i-j <= a[j]){
+//                 if(dp[i] == -1 || dp[i] > dp[j] +1){
+//                     dp[i] = dp[j] + 1;
+//                 }
+//             }
+//         }
+//     }
+//     cout << dp[n-1] << endl;
+
+// }
+
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -14,16 +44,20 @@ int main(){
         cin >> a[i];
     }
     vector<int> dp(n,-1);
+    
     dp[0] = 0;
-    for(int i=1; i<n; i++){
-        for(int j=0; j<i; j++){
-            if(dp[j] != -1 && i-j <= a[j]){
-                if(dp[i] == -1 || dp[i] > dp[j] +1){
-                    dp[i] = dp[j] + 1;
-                }
-            }
+
+    for(int i=0; i<n; i++){
+        if(dp[i] == -1) continue;
+        for(int j=i+1; j<=i+a[i]; j++){
+            if(j >= n) break;
+            if(dp[j] == -1 || dp[j] > dp[i] +1){
+                dp[j] = dp[i] +1;
+            } 
         }
     }
     cout << dp[n-1] << endl;
 
 }
+
+
